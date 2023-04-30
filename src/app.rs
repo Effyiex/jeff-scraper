acf6614c7f16adaa45c4ee156ac970b89217210f
Effyiex <leakforumse@gmail.com>
2023-04-30 21:58:09 +0200

    InDev #002

--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,17 @@
 
-use bevy::prelude::*;
-use bevy::winit::WinitWindows;
+use bevy::{
+  prelude::*,
+  winit::WinitWindows,
+  core_pipeline::clear_color::ClearColorConfig
+};
+
+use winit::{
+  dpi::{
+    PhysicalSize,
+    PhysicalPosition
+  },
+  window::WindowLevel
+};
 
 pub struct AppPlugin;
 pub struct AppToggleEvent;
@@ -20,33 +31,138 @@ impl Plugin for AppPlugin {
 
 }
 
+#[derive(Resource)]
+struct AppResources {
+  regular_font: Handle<Font>
+}
+
 fn setup(
+  mut commands: Commands,
   mut windows: Query<&mut Window>,
-  winit_buffer: NonSend<WinitWindows>
+  winit_buffer: NonSend<WinitWindows>,
+  asset_server: Res<AssetServer>
 ) {
 
   for mut window in windows.iter_mut() {
     window.title = format!("{} ({})", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
   }
+  println!("[{}::app::setup] // Set Window-Title.", env!("CARGO_PKG_NAME"));
 
   for window in winit_buffer.windows.iter() {
+
+    window.1.set_decorations(false);
+    window.1.set_window_level(WindowLevel::AlwaysOnTop);
     window.1.set_visible(false);
+    println!("[{}::app::setup] // Set Window-Defaults.", env!("CARGO_PKG_NAME"));
+
+    if let Some(monitor) = window.1.current_monitor() {
+      let screen_size: PhysicalSize<u32> = monitor.size();
+      window.1.set_inner_size(PhysicalSize::new(screen_size.width / 3, screen_size.height));
+      window.1.set_outer_position(PhysicalPosition::new(screen_size.width / 4 * 3, 0));
+    }
+    println!("[{}::app::setup] // Set Window-Boundaries.", env!("CARGO_PKG_NAME"));
+
   }
+
+  commands.spawn(Camera2dBundle {
+    camera_2d: Camera2d {
+      clear_color: ClearColorConfig::Custom(Color::rgb(0.05, 0.05, 0.075)) 
+    },
+    ..Default::default()
+  });
+  println!("[{}::app::setup] // Spawned Camera.", env!("CARGO_PKG_NAME"));
+
+  let resources: AppResources = AppResources { 
+    regular_font: asset_server.load("nunito_regular.ttf")
+  };
+
+  create_menu(&mut commands, &resources);
+
+  commands.insert_resource(resources);
 
 }
 
 fn await_toggle(
-    event_reader: EventReader<AppToggleEvent>,
+    mut event_reader: EventReader<AppToggleEvent>,
     winit_buffer: NonSend<WinitWindows>
 ) {
 
     if event_reader.is_empty() {
         return;
     }
+    event_reader.clear();
+    println!("[{}::app::await_toggle] // AppToggleEvent received.", env!("CARGO_PKG_NAME"));
 
     for window in winit_buffer.windows.iter() {
+
       window.1.set_visible(!window.1.is_visible().unwrap());
+
+      if window.1.is_visible().unwrap() {
+        println!("[{}::app::await_toggle] // Visible.", env!("CARGO_PKG_NAME"));
+      } else {
+        println!("[{}::app::await_toggle] // Invisible.", env!("CARGO_PKG_NAME"));
+      }
+
     }
 
+}
+
+fn create_menu(
+  commands: &mut Commands,
+  resources: &AppResources
+)  {
+  
+  commands.spawn(ImageBundle {
+    style: Style {
+      position_type: PositionType::Absolute,
+      size: Size::all(Val::Percent(100.0)),
+      align_items: AlignItems::Center,
+      flex_direction: FlexDirection::Column,
+      justify_content: JustifyContent::Start,
+      ..Default::default()
+    },
+    background_color: Color::rgb(0.05, 0.05, 0.075).into(),
+    ..Default::default()
+  })
+    .with_children(|parent| {
+
+      parent.spawn(ImageBundle {
+        style: Style {
+          position_type: PositionType::Absolute,
+          position: UiRect::top(Val::Px(0.0)),
+          size: Size::new(Val::Percent(100.0), Val::Px(64.0)),
+          ..Default::default()
+        },
+        background_color: Color::rgba(0.0, 0.0, 0.0, 0.25).into(),
+        ..Default::default()
+      })
+        .with_children(|parent| {
+
+          parent.spawn(TextBundle {
+            style: Style {
+              margin: UiRect { 
+                left: Val::Px(16.0), 
+                right: Val::Auto, 
+                top: Val::Auto, 
+                bottom: Val::Auto 
+              },
+              ..Default::default()
+            },
+            text: Text::from_section(
+              format!("{} ({})", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION")),
+              TextStyle {
+                font: resources.regular_font.clone(),
+                font_size: 48.0,
+                color: Color::WHITE
+              }
+            ),
+            ..Default::default()
+          });
+
+        });
+
+    });
+
+  println!("[{}::app::create_menu] // Menu created.", env!("CARGO_PKG_NAME"));
 
 }
--- src/keybind.rs
+++ src/keybind.rs
@@ -1,37 +1,139 @@
 
 use crate::app::AppToggleEvent;
 
+use std::{
+  collections::HashMap,
+  path::Path,
+  fs::{
+    File,
+    self
+  }
+};
+
 use bevy::prelude::*;
 
-use inputbot::{
-  BlockInput,
-  KeybdKey::*
-};
+use inputbot::KeybdKey;
 
 pub struct KeybindPlugin;
 
 impl Plugin for KeybindPlugin {
 
   fn build(&self, app: &mut App) {
-
-    app.add_system(await_input);
-
-    LControlKey.blockable_bind(move || {
-      if LShiftKey.is_pressed() && GKey.is_pressed() {
-        BlockInput::Block
-      } else {
-        BlockInput::DontBlock
-      }
-    });
-
+    app
+      .add_system(await_input)
+      .add_startup_system(setup);
   }
 
 }
 
-fn await_input(
-  mut app_toggler: EventWriter<AppToggleEvent>
-) {
-  if LControlKey.is_pressed() && LShiftKey.is_pressed() && GKey.is_pressed() {
-    app_toggler.send(AppToggleEvent);
+#[derive(Component)]
+struct KeybindComponent {
+  binds: Vec<&'static KeybdKey>,
+  status: usize
+}
+
+fn setup(mut commands: Commands) {
+  
+  let mut component: KeybindComponent = KeybindComponent { 
+    binds: vec![],
+    status: 0
+  };
+
+  let config_path: &Path = Path::new("./keybind.cfg");
+  if config_path.exists() && config_path.is_file() {
+    match fs::read_to_string(config_path) {
+      Ok(config_content) => {
+        for line in config_content.lines() {
+          for bind in line.replace(" ", "").trim().split(',') {
+
+            if bind.is_empty() || !bind.is_ascii() {
+              continue;
+            }
+
+            if let Some(key) = get_key_from_string(bind.to_string()) {
+              component.binds.push(key);
+            }
+
+          }
+        }
+        println!("[{}::keybind::setup] // Interpreted from Keybind-Config.", env!("CARGO_PKG_NAME"));
+      },
+      _ => {
+        println!("[{}::keybind::setup] // Couldn't read from Keybind-Config.", env!("CARGO_PKG_NAME"));
+      }
+    }
+  } else {
+    File::create(config_path).unwrap();
+    println!("[{}::keybind::setup] // Created Keybind-Config-File.", env!("CARGO_PKG_NAME"));
   }
+
+  commands.spawn_empty().insert(component);
+
+}
+
+fn await_input(
+  mut app_toggler: EventWriter<AppToggleEvent>,
+  mut component_query: Query<&mut KeybindComponent>
+) {
+
+  let mut component = component_query.single_mut();
+
+  let mut latest_status: usize = 0;
+  for bind in &component.binds {
+    if bind.is_pressed() {
+      latest_status += 1;
+    }
+  }
+
+  if component.binds.len() <= latest_status && latest_status > component.status  {
+    app_toggler.send(AppToggleEvent);
+    println!("[{}::keybind::await_input] // AppToggleEvent sent.", env!("CARGO_PKG_NAME"));
+  }
+
+  component.status = latest_status;
+
+}
+
+fn get_key_from_string(id: String) -> Option<&'static KeybdKey> {
+
+  println!("[{}::keybind::get_key] // Searching KeyObject of: \"{}\".", env!("CARGO_PKG_NAME"), id);
+  
+  let mut keys: HashMap<String, &KeybdKey> = HashMap::new();
+  keys.insert("A".to_string(), &KeybdKey::AKey);
+  keys.insert("B".to_string(), &KeybdKey::BKey);
+  keys.insert("C".to_string(), &KeybdKey::CKey);
+  keys.insert("D".to_string(), &KeybdKey::DKey);
+  keys.insert("E".to_string(), &KeybdKey::EKey);
+  keys.insert("F".to_string(), &KeybdKey::FKey);
+  keys.insert("G".to_string(), &KeybdKey::GKey);
+  keys.insert("H".to_string(), &KeybdKey::HKey);
+  keys.insert("I".to_string(), &KeybdKey::IKey);
+  keys.insert("J".to_string(), &KeybdKey::JKey);
+  keys.insert("K".to_string(), &KeybdKey::KKey);
+  keys.insert("L".to_string(), &KeybdKey::LKey);
+  keys.insert("M".to_string(), &KeybdKey::MKey);
+  keys.insert("N".to_string(), &KeybdKey::NKey);
+  keys.insert("O".to_string(), &KeybdKey::OKey);
+  keys.insert("P".to_string(), &KeybdKey::PKey);
+  keys.insert("Q".to_string(), &KeybdKey::QKey);
+  keys.insert("R".to_string(), &KeybdKey::RKey);
+  keys.insert("S".to_string(), &KeybdKey::SKey);
+  keys.insert("T".to_string(), &KeybdKey::TKey);
+  keys.insert("U".to_string(), &KeybdKey::UKey);
+  keys.insert("V".to_string(), &KeybdKey::VKey);
+  keys.insert("W".to_string(), &KeybdKey::WKey);
+  keys.insert("X".to_string(), &KeybdKey::XKey);
+  keys.insert("Y".to_string(), &KeybdKey::YKey);
+  keys.insert("Z".to_string(), &KeybdKey::ZKey);
+  keys.insert("LCONTROL".to_string(), &KeybdKey::LControlKey);
+  keys.insert("RCONTROL".to_string(), &KeybdKey::RControlKey);
+  keys.insert("LSHIFT".to_string(), &KeybdKey::LShiftKey);
+  keys.insert("RSHIFT".to_string(), &KeybdKey::RShiftKey);
+  keys.insert("ENTER".to_string(), &KeybdKey::EnterKey);
+  keys.insert("ESCAPE".to_string(), &KeybdKey::EscapeKey);
+  keys.insert("BACKSPACE".to_string(), &KeybdKey::BackspaceKey);
+  keys.insert("DELETE".to_string(), &KeybdKey::DeleteKey);
+
+  keys.get(&id.to_uppercase()).copied()
+
 }
